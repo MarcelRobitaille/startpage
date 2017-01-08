653798d9782ffc4dfc2cea36fc41341eb5bd01d8
Marcel Robitaille <marcelrobitaille11@gmail.com>
2017-01-07 23:32:41 -0400

    Use cache busted assets

--- tests/support/jsdom.js
+++ tests/support/jsdom.js
@@ -1,14 +1,17 @@
 'use strict'
 
-const path = require('path')
 const fs = require('fs')
+const path = require('path')
+const load = (file) => require(path.join(__dirname, file))
 
 const jsdom = require('jsdom').jsdom
 const pug = require('pug')
 
+const pugfn = load('../../functions/pugfn.js')
+
 const filename = path.join(__dirname, '../../views/index.pug')
 const template = fs.readFileSync(filename, 'utf-8')
-const html = pug.compile(template, { filename })({ })
+const html = pug.compile(template, { filename })(pugfn)
 
 module.exports = () => {
   if(typeof document !== 'undefined') return
