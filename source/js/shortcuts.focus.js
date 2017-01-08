93b1fd1b1c764090d84a2dc7c850d84eeb4ff477
Marcel Robitaille <marcelrobitaille11@gmail.com>
2017-01-07 23:30:49 -0400

    Remove focus before resetting it

--- source/js/shortcuts.focus.js
+++ source/js/shortcuts.focus.js
@@ -9,16 +9,15 @@ const self = {
   level: () => $focus ? parseInt($focus.getAttribute('data-level')) : -1,
 
   set: (_$focus) => {
+    self.remove()
     $focus = _$focus
     $focus.classList.add('focus')
   },
 
   remove: () => {
-    if($focus){
-      $focus.classList.remove('focus')
-      $focus = null
-    }
-    return 'focus.remove'
+    if(!$focus) return
+    $focus.classList.remove('focus')
+    $focus = null
   },
 
 }
